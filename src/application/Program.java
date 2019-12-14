950f7b549af3cfc2edae107fc18f0071cc69b57a
Eric Araújo <eric.araujolima@hotmail.com>
2019-12-13 23:33:41 -0300

    Exceção de erro e estrutura do código corrigidos

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,49 +16,48 @@ import entities.Product;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("Digite o caminho do arquivo .CSV que deseja ler: ");
 		String path = sc.nextLine();
-		
+
 		List<Product> products = new ArrayList<>();
-		
+
 		File parentPath = new File(path);
-		
-		new File (parentPath.getParent() + "\\out").mkdir();
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))){
+
+		new File(parentPath.getParent() + "\\out").mkdir();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String[] valuesLine = null;
 			String line = br.readLine();
-			while(line != null) {
-				System.out.println(line);
-				
+			while (line != null) {
+
 				valuesLine = line.split(",");
-				
+
 				String name = valuesLine[0];
 				double value = Double.parseDouble(valuesLine[1]);
 				int quantity = Integer.parseInt(valuesLine[2]);
-				
+
 				products.add(new Product(name, value, quantity));
-				
-				try (BufferedWriter bw = new BufferedWriter(new FileWriter(parentPath.getParent() + "\\out\\summary.csv"))){
-					for (Product product : products) {
-						bw.write(product.toString());
-						bw.newLine();
-					}
-				}catch(IOException e) {
-					e.printStackTrace();
-				}
-				
+
 				line = br.readLine();
 			}
-			
-		}catch(IOException e) {
-			e.printStackTrace();
+			try (BufferedWriter bw = new BufferedWriter(
+					new FileWriter(parentPath.getParent() + "\\out\\summary.csv"))) {
+				for (Product product : products) {
+					bw.write(product.toString());
+					bw.newLine();
+				}
+			} catch (IOException e) {
+				System.out.print("Erro na gravação do arquivo: " + e.getMessage());
+			}
+
+		} catch (IOException e) {
+			System.out.print("Erro na leitura do arquivo: " + e.getMessage());
 		}
-		
+
 		sc.close();
 	}
 
