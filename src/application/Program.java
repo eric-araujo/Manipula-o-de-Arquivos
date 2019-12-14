37f84d84e56d9c0476eb40193291e55524f40501
Eric Araújo <eric.araujolima@hotmail.com>
2019-12-13 22:39:33 -0300

    Fazendo leitura dos arquivos solicitados pelo usuário

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,6 +12,21 @@ public class Program {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite o caminho do arquivo .CSV que deseja ler: ");
+		String path = sc.nextLine();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
+			
+			String line = br.readLine();
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
 		
 		sc.close();
 	}
